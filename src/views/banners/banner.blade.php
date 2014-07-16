a2fd32975fb9d15d56bff39378f81ccb4a19e58c
SolverCircle <developers@solvercircle.com>
2014-07-16 15:06:40 +0600

    view changes

--- src/views/banners/banner.blade.php
+++ src/views/banners/banner.blade.php
@@ -7,5 +7,7 @@
 @stop
 
 @section('content')
+<div style="width:{{ Solvercircle\Scbanner\SCBSSettings::get_setting(Config::get('Scbanner::config.key_banner_width')) }}px">
 {{ Solvercircle\Scbanner\BannerUI::display(); }}
+</div>
 @stop
